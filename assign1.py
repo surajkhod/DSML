43a178c58df7fd1590c8b96bfbe4e0078eb8d728
surajkhod <surajkhod234@gmail.com>
2024-11-24 18:38:16 +0530

    add Q1

--- assign1.py
+++ assign1.py
@@ -1,1 +1,5 @@
-print("Hello")
+# Perform the following operations using Python on a data set : read data
+# from different formats(like csv, xls),indexing and selecting data, sort data,
+# describe attributes of data, checking data types of each column. (Use
+# Titanic Dataset).
+
